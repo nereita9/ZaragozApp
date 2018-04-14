533b92d34227c299efb55bbd7d8283cb302f8a35
Nerea Gonzalez Vazquez <nerea.gonzalez.vazquez@everis.com>
2018-04-14 12:40:51 +0200

    Initial Commit

---
+++ ZaragozApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ZaragozApp
+//
+//  Created by Nerea Gonzalez Vazquez on 14/04/2018.
+//  Copyright © 2018 Nerea Gonzalez Vazquez. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ZaragozApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ZaragozApp
+//
+//  Created by Nerea Gonzalez Vazquez on 14/04/2018.
+//  Copyright © 2018 Nerea Gonzalez Vazquez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
